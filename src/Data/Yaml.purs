e491ec7ff16c7e5a78e3797e6218f6ab42786cda
nobv <6e6f6275@gmail.com>
2019-07-04 09:49:33 +0900

    Added types to map with yaml

--- src/Data/Yaml.purs
+++ src/Data/Yaml.purs
@@ -5,6 +5,35 @@ import Data.Function.Uncurried (Fn1, runFn1)
 
 foreign import _safeLoad :: Fn1 String String
 
+data Kind = Goto | Input | Submit | Click
+
+type Dstp =
+  { setting :: Setting
+  , routes  :: Array Difinitions
+  }
+
+type Setting =
+  { headless :: Boolean
+  , debug    :: Boolean
+  }
+
+type Difinitions =
+  { name    :: String
+  , baseUrl :: String
+  , enabled :: Boolean
+  -- todo: multipule types
+  , route   :: Array Goto
+  }
+
+type Goto = { url :: String }
+
+type Input = { field :: Array Field }
+
+type Field =
+  { selector :: String
+  , value    :: String
+  }
+
 safeLoad :: String -> String
 safeLoad yaml = do
   runFn1 _safeLoad yaml
