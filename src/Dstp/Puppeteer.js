e49b262f04892f7910fd652959cfb85b034923e8
Robert P <robertdp@users.noreply.github.com>
2019-09-12 20:16:21 +0900

    Fixed FFI functions

--- src/Dstp/Puppeteer.js
+++ src/Dstp/Puppeteer.js
@@ -8,32 +8,24 @@ exports._newPage = function(browser) {
   return browser.newPage();
 };
 
-exports._goto = function(page) {
-  return function(url) {
-    return page.goto(url);
-  };
+exports._goto = function(page, url) {
+  return page.goto(url);
 };
 
 exports._close = function(browser) {
   return browser.close();
 };
 
-exports._click = function(page) {
-  return function(selector) {
-    return page.click(selector);
-  };
+exports._click = function(page, selector) {
+  return page.click(selector);
 };
 
-exports._screenshot = function(page) {
-  return function(options) {
-    return page.screenshot(options);
-  };
+exports._screenshot = function(page, options) {
+  return page.screenshot(options);
 };
 
-exports._submit = function(page) {
-  return function(selector) {
-    return page.evaluate("$(" + selector + ").parent('form').submit()");
-  };
+exports._submit = function(page, selector) {
+  return page.evaluate("$(" + selector + ").parent('form').submit()");
 };
 
 exports._waitForNavigation = function(page) {
@@ -41,8 +33,6 @@ exports._waitForNavigation = function(page) {
 };
 
 
-exports._waitForSelector = function(page) {
-  return function (selector) {
-    return page.waitForSelector(selector);
-  };
+exports._waitForSelector = function(page, selector) {
+  return page.waitForSelector(selector);
 };
